5b0ed825bbde28140f33a9a1ff14e748bbbd98a0
Evaldo Gomes <evaldo_gomes@hotmail.com>
2020-08-28 00:35:20 -0300

    Aula 5-3-2 Plugin Slide

---
+++ web/js/script.js
@@ -0,0 +1,12 @@
+if (window.SimpleSlide) {
+    new SimpleSlide({
+        slide: "quote", // nome do atributo data-slide="quote"
+        time: 5000, // tempo de transição dos slides
+    });
+
+    new SimpleSlide({
+        slide: "portifolio",
+        time: 5000,
+        nav: true,
+    });
+}
